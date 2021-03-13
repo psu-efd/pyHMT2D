6d1fedfc27de853a07fd11f2ae180d68cb3778a8
psuliuxf <psuefd@gmail.com>
2021-03-13 18:25:41 -0500

    added HEC_RAS_Model.py

--- pyHMT2D/RAS_2D/__init__.py
+++ pyHMT2D/RAS_2D/__init__.py
@@ -1,3 +1,5 @@
 from .RAS_2D_Data import RAS_2D_Data
+from .HEC_RAS_Model import HEC_RAS_Model
+from .helpers import *
 
-__all__ = ["RAS_2D_Data"]
+__all__ = ["RAS_2D_Data", "HEC_RAS_Model"]
--- pyHMT2D/__init__.py
+++ pyHMT2D/__init__.py
@@ -4,6 +4,7 @@ from . import (
     Misc
 )
 from .__about__ import __version__
+from .__common__ import *
 
 __all__ = [
     "SRH_2D",
