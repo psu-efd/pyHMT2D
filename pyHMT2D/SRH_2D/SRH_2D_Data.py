040fb5abf039df489a2a58c42eb994c30510b378
psuliuxf <psuefd@gmail.com>
2021-03-20 09:12:15 -0400

    added Manning's n output to VTK; before removing dependency on meshio

--- pyHMT2D/SRH_2D/SRH_2D_Data.py
+++ pyHMT2D/SRH_2D/SRH_2D_Data.py
@@ -6,6 +6,8 @@ Penn State University
 """
 
 import sys
+import os
+import copy
 import numpy as np
 import h5py
 import meshio
@@ -30,6 +32,9 @@ vtkCellTypeMap = {
 
 # maximum number of nodes for an element
 gMax_Nodes_per_Element = 8
+
+# maximum number of elements for a node
+gMax_Elements_per_Node = 10
 
 class SRH_2D_SRHHydro:
     """A class to handle srhhydro file for SRH-2D
@@ -79,7 +84,7 @@ class SRH_2D_SRHHydro:
                 continue
 
             if parts[0] == 'ManningsN':
-                res_ManningsN[int(parts[1])] = parts[2]
+                res_ManningsN[int(parts[1])] = float(parts[2])
             elif parts[0] == 'BC':
                 res_BC[int(parts[1])] = parts[2]
             elif parts[0] == 'IQParams':
@@ -298,12 +303,24 @@ class SRH_2D_SRHGeom:
         # each node's 3D coordinates
         self.nodeCoordinates = np.zeros([self.numOfNodes, 3], dtype=np.float64)
 
+        # each element (cell)'s bed elevation (z)
+        self.elementBedElevation = np.zeros(self.numOfElements, dtype=np.float64)
+
         # each NodeString's list of nodes (stored in a dictionary)
         self.nodeStringsDict = {}
 
         # get the mesh information (elementNodesList, elementNodesCount, vtkCellTypeCode, and nodeCoordinates)
         # from reading the SRHGEOM file again
         self.readSRHGEOMFile()
+
+        # list of elements for all nodes
+        self.nodeElementsList = np.zeros([self.numOfNodes, gMax_Elements_per_Node], dtype=int)
+
+        # number of elements for each node (1,2,...,gMax_Elements_per_Node)
+        self.nodeElementsCount = np.zeros(self.numOfNodes, dtype=int)
+
+        # build the element list for each node
+        self.buildNodeElements()
 
 
     def getNumOfElementsNodes(self):
@@ -423,12 +440,51 @@ class SRH_2D_SRHGeom:
 
         srhgeomfile.close()
 
+        #calculate the bed elevation at cell center
+        for cellI in range(self.numOfElements):
+
+            elev_temp = 0.0
+
+            #loop over all nodes of current element
+            for nodeI in range(self.elementNodesCount[cellI]):
+                elev_temp += self.nodeCoordinates[self.elementNodesList[cellI][nodeI]-1,2]  #z elevation; nodeI-1 because node number is 1-based for SRH-2D
+
+            self.elementBedElevation[cellI] = elev_temp / self.elementNodesCount[cellI]
+
         if False:
             print("elementNodesList = ", self.elementNodesList)
             print("elementNodesCount = ", self.elementNodesCount)
             print("vtkCellTypeCode = ", self.vtkCellTypeCode)
             print("nodeCoordinates = ", self.nodeCoordinates)
+            print("elementBedElevation = ", self.elementBedElevation)
             print("nodeStrings = ", self.nodeStringsDict)
+
+    def buildNodeElements(self):
+        """ Build node's element list for all nodes
+
+        Returns
+        -------
+
+        """
+
+        #create an emptp list of lists for all nodes
+        self.nodeElementsList = [[] for _ in range(self.numOfNodes)]
+
+        #loop over all cells
+        for cellI in range(self.numOfElements):
+            #loop over all nodes of current cell
+            for i in range(self.elementNodesCount[cellI]):
+                #get the node ID of current node
+                nodeID = self.elementNodesList[cellI][i]
+
+                self.nodeElementsList[nodeID-1].append(cellI)  #nodeID-1 because SRH-2D is 1-based
+
+        #count beans in each node's element list
+        for nodeI in range(self.numOfNodes):
+            self.nodeElementsCount[nodeI] = len(self.nodeElementsList[nodeI])
+
+        #print("nodeElementsCount = ", self.nodeElementsCount)
+        #print("nodeElementsList = ", self.nodeElementsList)
 
 class SRH_2D_SRHMat:
     """A class to handle srhmat file for SRH-2D
@@ -451,6 +507,7 @@ class SRH_2D_SRHMat:
         #each material zone's cell list (in a dictionary: material zone ID and cell list)
         self.matZoneCells = {}
 
+        #build material zones data
         self.buildMaterialZonesData()
 
     def buildMaterialZonesData(self):
@@ -489,37 +546,75 @@ class SRH_2D_SRHMat:
             search = line.split()
             # print(search)
 
+#            print("search[0] == Material", (search[0] == "Material"))
 
-            if len(search) != 0:
+            if len(search) != 0: #if it is not just an empty line
                 if search[0] == "SRHMAT":
                     continue
-
-                if search[0] == "NMaterials":
-                    self.numOfMaterials = search[1]
-
-                if search[0] == "MatName":
+                elif search[0] == "NMaterials":
+                    self.numOfMaterials = int(search[1])
+                elif search[0] == "MatName":
                     res_MatNames[search[1]] = search[2]
-
-                if search[0] == "Material": # a new material zone starts
+                elif search[0] == "Material": # a new material zone starts
                     #if this is not the first Material zone; save the previous Material zone
-                    if (current_MaterialID != -1) and (not current_MaterialCellList):
-                        res_Materials[current_MaterialID] = current_MaterialCellList
+                    if ((current_MaterialID != -1) and (len(current_MaterialCellList)!=0)):
+                        res_Materials[current_MaterialID] = copy.deepcopy(current_MaterialCellList)
 
                         #clear up current_MaterialID and current_MaterialCellList
                         current_MaterialID = -1
                         current_MaterialCellList.clear()
 
-                    current_MaterialID = search[1]
-                    current_MaterialCellList.append(search[2:])
-
+                    current_MaterialID = int(search[1])
+                    current_MaterialCellList.extend(search[2:])
                 else: #still in a material zone (assume there is no other things other than those listed above in the
                       #srhmat file)
-                    current_MaterialCellList.append(search)
+                    current_MaterialCellList.extend(search)
+
+            #add the last material zone
+            res_Materials[current_MaterialID] = copy.deepcopy(current_MaterialCellList)
 
         srhmatfile.close()
 
+        #convert material ID from str to int
+        for zoneID, cellList in res_Materials.items():
+            print("cellList ", cellList)
+            temp_list = [int(i) for i in cellList]
+            res_Materials[zoneID] = temp_list
+
         self.matNameList = res_MatNames
         self.matZoneCells = res_Materials
+
+        #print("matNameList = ", self.matNameList)
+        #print("matZoneCells = ", self.matZoneCells)
+
+
+    def find_cell_material_ID(self, cellID):
+        """ Given a cell ID, find its material (Manning's n) ID
+
+        Parameters
+        ----------
+        cellID:
+
+        Returns
+        -------
+
+        """
+
+        if not self.matZoneCells:
+            self.buildMaterialZonesData()
+
+        #whether the cell is found in the list
+        bFound = False
+
+        for matID, cellList in self.matZoneCells.items():
+            if (cellID+1) in cellList:  #cellID+1 because SRH-2D is 1-based
+                bFound = True
+                return matID
+
+        #if cellID is not found, report problem
+        if not bFound:
+            print("In find_cell_material_ID(cellID), cellID =", cellID, "is not found. Check mesh. Exiting ...")
+            sys.exit()
 
 
 class SRH_2D_Data:
@@ -558,6 +653,13 @@ class SRH_2D_Data:
         self.srhgeom_obj = SRH_2D_SRHGeom(self.srhgeom_filename)
         self.srhmat_obj = SRH_2D_SRHMat(self.srhmat_filename)
 
+        #Manning's n value at cell centers and nodes
+        self.ManningN_cell = np.zeros(self.srhgeom_obj.numOfElements, dtype=float)
+        self.ManningN_node = np.zeros(self.srhgeom_obj.numOfNodes, dtype=float)
+
+        #build Manning's n value at cell centers and nodes
+        self.buildManningN_cells_nodes()
+
         #XMDF data: if desired, the result data in a XMDF file
         #can be read in and stored in the following arrays.
         #Depending on whether the XMDF is nodal or at cell centers, use different arrays.
@@ -576,6 +678,37 @@ class SRH_2D_Data:
 
         """
         return self.srhhydro_obj.srhhydro_content["Case"]
+
+    def buildManningN_cells_nodes(self):
+        """ Build Manning's n values at cell centers and nodes
+
+        This calculation is based on the srhhydro and srhgeom files, not interpolation from a GeoTiff file.
+
+        Returns
+        -------
+
+        """
+
+        print("Building Manning's n values for cells and nodes in SRH-2D mesh ...")
+
+        #Manning's n dictionary in srhhydro
+        nDict = self.srhhydro_obj.srhhydro_content["ManningsN"]
+        print("nDict = ", nDict)
+
+        #loop over all cells in the mesh
+        for cellI in range(self.srhgeom_obj.numOfElements):
+            #get the material ID of current cell
+            matID = self.srhmat_obj.find_cell_material_ID(cellI)
+
+            #print("matID = ", matID)
+            #print("nDict[matID] = ", nDict[matID])
+
+            #get the Manning's n value for current cell
+            self.ManningN_cell[cellI] = nDict[matID]
+
+        #interpolate Manning's n from cell centers to nodes
+
+
 
     def readSRHXMDFFile(self, xmdfFileName, bNodal):
         """ Read SRH-2D result file in XMDF format (current version 13.1.6 of SMS only support data at node).
@@ -661,6 +794,13 @@ class SRH_2D_Data:
                 else:
                     self.xmdfAllData_Nodal[varName] = np.array(xmdfFile[varName]['Values'])
 
+                    #fixe the water elevation = -999 in SRH-2D
+                    if varName == "Water_Elev_ft" or varName == "Water_Elev_m":
+                        for nodeI in range(len(self.xmdfAllData_Nodal[varName])):
+                            if self.xmdfAllData_Nodal[varName][nodeI] == -999:
+                                self.xmdfAllData_Nodal[varName][nodeI] = self.srhgeom_obj.nodeCoordinates[nodeI, 2] #use node elevation
+
+
                 if np.array(xmdfFile[varName]['Values']).shape[1] != self.srhgeom_obj.numOfNodes:
                     print("The number of nodes in the XMDF file (%d) is different from that in the mesh (%d). "
                           "Abort readSRHXMDFFile(...) function."
@@ -679,6 +819,14 @@ class SRH_2D_Data:
                 else:
                     self.xmdfAllData_Cell[varName] = np.array(xmdfFile[varName]['Values'])
 
+                    #fixe the water elevation = -999 in SRH-2D
+                    if varName == "Water_Elev_ft" or varName == "Water_Elev_m":
+                        #loop through time
+                        for timeI in range(self.xmdfAllData_Cell[varName].shape[0]):
+                            for cellI in range(self.xmdfAllData_Cell[varName].shape[1]):
+                                if self.xmdfAllData_Cell[varName][timeI, cellI] == -999:
+                                    self.xmdfAllData_Cell[varName][timeI, cellI] = self.srhgeom_obj.elementBedElevation[cellI]
+
                 if np.array(xmdfFile[varName]['Values']).shape[1] != self.srhgeom_obj.numOfElements:
                     print("The number of elements in the XMDF file (%d) is different from that in the mesh (%d). "
                           "Abort readSRHXMDFFile(...) function."
@@ -688,7 +836,7 @@ class SRH_2D_Data:
 
         xmdfFile.close()
 
-    def outputXMDFDataToVTK(self, bNodal, dir=''):
+    def outputXMDFDataToVTK(self, bNodal, timeStep=-1,lastTimeStep=False, dir=''):
         """Output XMDF result data to VTK
 
         This has to be called after the XMDF data have been loaded by calling readSRHXMDFFile(...)
@@ -697,6 +845,8 @@ class SRH_2D_Data:
 
         Attributes:
           bNodal: {bool} -- whether export nodal data or cell center data. Currently, it can't output both.
+          timeStep: {int} -- optionly only the specified time step will be saved
+          lastTimeStep: {bool} -- optionally specify only the last time step
           dir: {string} -- optional directory
 
         Returns
@@ -709,24 +859,55 @@ class SRH_2D_Data:
             print("Empty XMDF data arrays. Call readSRHXMDFFile() function first. Exiting ...")
             sys.exit()
 
+        #check the sanity of timeStep
+        if (timeStep != -1):
+            if bNodal:
+                if (not timeStep in range(len(self.xmdfTimeArray_Nodal))):
+                    message = "Specified timeStep = %d not in range (0 to %d)." % (timeStep, len(self.xmdfTimeArray_Nodal))
+                    sys.exit(message)
+            else:
+                if (not timeStep in range(len(self.xmdfTimeArray_Cell))):
+                    message = "Specified timeStep = %d not in range (0 to %d)." % (timeStep, len(self.xmdfTimeArray_Cell))
+                    sys.exit(message)
+
         vtkFileName_base = ''
         print("self.srhhydro_obj.srhhydro_content[\"Case\"] = ", self.srhhydro_obj.srhhydro_content["Case"])
 
         if dir!='':
-            vtkFileName_base = dir + '/' + self.srhhydro_obj.srhhydro_content["Case"] #use the case name of the
+            vtkFileName_base = dir + '/' + 'SRH2D_' + self.srhhydro_obj.srhhydro_content["Case"] #use the case name of the
             # SRH-2D case
         else:
-            vtkFileName_base = self.srhhydro_obj.srhhydro_content["Case"]  # use the case name of the SRH-2D case
+            vtkFileName_base = 'SRH2D_' + self.srhhydro_obj.srhhydro_content["Case"]  # use the case name of the SRH-2D case
         print("vtkFileName_base = ", vtkFileName_base)
 
         #build result variable names
         resultVarNames = list(self.xmdfAllData_Nodal.keys()) if bNodal else list(self.xmdfAllData_Cell.keys())
 
+        #add bed elevation
+        #get the units of the results
+        units = self.srhhydro_obj.srhhydro_content['OutputFormat'][1]
+        print("SRH-2D result units:", units)
+        if units == "SI":
+            resultVarNames.append("Bed_Elev_m")
+        else:
+            resultVarNames.append("Bed_Elev_ft")
+
         print("All solution variable names: ", resultVarNames)
+
+        #add Manning's n
+        resultVarNames.append("ManningN")
 
         #loop through each time step
         timeArray = self.xmdfTimeArray_Nodal if bNodal else self.xmdfTimeArray_Cell
         for timeI in range(timeArray.shape[0]):
+
+            if lastTimeStep:
+                if timeI < (timeArray.shape[0] - 1):
+                    continue
+
+            if (timeStep != -1) and (timeI != timeStep):
+                continue
+
             print("timeI and time = ", timeI, timeArray[timeI])
 
             #numpy array for all solution variables at one time
@@ -739,11 +920,20 @@ class SRH_2D_Data:
             print("vtkFileName = ", vtkFileName)
 
             #loop through each solution variable
-            for varName, varI in zip(resultVarNames, range(len(resultVarNames))):
+            for varName, varI in zip(resultVarNames, range(len(resultVarNames)-1)):
                 print("varName = ", varName)
-                #get the values of current solution varialbe at current time
-                resultData[:,varI] =      self.xmdfAllData_Nodal[varName][timeI,:] if bNodal \
-                                     else self.xmdfAllData_Cell[varName][timeI,:]
+                if ("Bed_Elev" not in varName) and ("ManningN" not in varName):
+                    #get the values of current solution varialbe at current time
+                    resultData[:,varI] =      self.xmdfAllData_Nodal[varName][timeI,:] if bNodal \
+                                         else self.xmdfAllData_Cell[varName][timeI,:]
+
+            #add the bed elevation (the last one in resultData)
+            resultData[:,len(resultVarNames)-2] = self.srhgeom_obj.nodeCoordinates[:,2] if bNodal \
+                                     else self.srhgeom_obj.elementBedElevation
+
+            #add Mannng's n
+            resultData[:, len(resultVarNames)-1] = self.ManningN_node if bNodal \
+                                    else self.ManningN_cell
 
             #call the vtk output function
             self.outputVTK(vtkFileName, resultVarNames, resultData, bNodal)
@@ -812,6 +1002,7 @@ class SRH_2D_Data:
         resultData: result data
         bNodal: whether the data is nodal (True) or at cell center (False)
 
+
         Returns
         -------
 
@@ -824,6 +1015,10 @@ class SRH_2D_Data:
         except IOError:
             print('vtk file open error')
             sys.exit()
+
+        #get the units of the results
+        units = self.srhhydro_obj.srhhydro_content['OutputFormat'][1]
+        print("SRH-2D result units:", units)
 
         fid.write('# vtk DataFile Version 3.0\n')
         fid.write('Results from SRH-2D Modeling Run\n')
@@ -905,7 +1100,10 @@ class SRH_2D_Data:
             # Then output Vel_X and Vel_Y as velocity vector (Vel_Z = 0.0)
             # print('nColVel_X, nColVel_Y = %d, %d' % (nColVel_X, nColVel_Y))
             if (nColVel_X != -1) and (nColVel_Y != -1):
-                fid.write('VECTORS velocity double \n')
+                if units == "SI":
+                    fid.write('VECTORS Velocity_m_p_s double \n')
+                else:
+                    fid.write('VECTORS Velocity_ft_p_s double \n')
 
                 for entryI in range(entryCount):
                     fid.write('%f %f 0.0   ' % (resultData[entryI][nColVel_X],
